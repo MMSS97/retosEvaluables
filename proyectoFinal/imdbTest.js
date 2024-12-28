4e77fb6bd86be09e831487fbc79d3e1dc9f4ffd2
MMSS97 <michaelsorrosa@gmail.com>
2024-12-28 14:01:08 +0100

    witeFileSync

--- proyectoFinal/imdbTest.js
+++ proyectoFinal/imdbTest.js
@@ -5,3 +5,5 @@ var movieTest_1 = require("./movieTest");
 var imdb1 = new imdb_1.imdb(movieTest_1.myMovies);
 console.log(imdb1.showData());
 console.log(JSON.stringify(imdb1));
+var fs_1 = require("fs");
+(0, fs_1.writeFileSync)("imdbBBDD.json", JSON.stringify(imdb1));
--- proyectoFinal/imdbTest.ts
+++ proyectoFinal/imdbTest.ts
@@ -4,4 +4,10 @@ import { myMovies } from "./movieTest";
 const imdb1= new imdb(myMovies)
 console.log(imdb1.showData())
 console.log(JSON.stringify(imdb1));
+import {writeFileSync, readFileSync} from "fs"; 
+writeFileSync("imdbBBDD.json", JSON.stringify(imdb1))
+let fs = require('fs');
+fs.readFileSync
+
+
 
